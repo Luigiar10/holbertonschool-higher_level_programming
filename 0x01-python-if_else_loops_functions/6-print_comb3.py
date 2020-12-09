fb0e68cea9dc8ab397badee4d2074d09b5d12b48
Luigi Arrieta <luigi10arrieta@gmail.com>
2020-12-08 22:03:53 -0500

    Fix Comb 3

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,8 +2,8 @@
 for x in range(10):
     for i in range(10):
         if i > x:
-            print("{}".format(i), end="")
+            print('{}'.format(x), end='')
             if x != 8 or i != 9:
-                print("{}".format(i), end=", ")
+                print('{}'.format(i), end=', ')
             else:
-                print("{}".format(x))
+                print('{}'.format(i))
