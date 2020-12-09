e07ae53a4b09d8535f9e1d82f410948a2ce98431
Luigi Arrieta <luigi10arrieta@gmail.com>
2020-12-08 21:57:51 -0500

    Print Comb 3

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+for x in range(10):
+    for i in range(10):
+        if i > x:
+            print("{}".format(i), end="")
+            if x != 8 or i != 9:
+                print("{}".format(i), end=", ")
+            else:
+                print("{}".format(x))
