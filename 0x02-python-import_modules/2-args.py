eaab60773db8ec238e064a8247dc4f5fcd6d224b
Luigi Arrieta <luigi10arrieta@gmail.com>
2020-12-10 20:30:35 -0500

    Fix Arguments List

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     if len(argv) == 1:
         print("0 arguments")
     elif len(argv) == 2:
-        print("1 argument")
+        print("1 arguments")
     else:
         print('{} arguments:'.format(len(argv) - 1))
     for i in argv[1:]:
