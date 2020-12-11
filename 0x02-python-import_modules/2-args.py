6a6fbd972d168944df315f973e912db2219624b5
Luigi Arrieta <luigi10arrieta@gmail.com>
2020-12-10 20:33:41 -0500

    Fix3 Arguments List

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,11 +2,13 @@
 if __name__ == "__main__":
     from sys import argv
     if len(argv) == 1:
-        print("0 arguments")
-    elif len(argv) == 2:
-        print("1 arguments")
+        print('0 arguments.')
     else:
-        print('{} arguments:'.format(len(argv) - 1))
-    for i in argv[1:]:
-        print('{}: {}'.format(index, i))
-        index += 1
+        index = 1
+        if len(argv) == 2:
+            print('1 argument:')
+        else:
+            print('{} arguments:'.format(len(argv) - 1))
+        for i in argv[1:]:
+            print('{}: {}'.format(index, i))
+            index += 1
