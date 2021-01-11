8302bb49d485cf43dfef7492618d00155020422e
Luigi Arrieta <luigi10arrieta@gmail.com>
2021-01-11 17:42:24 +0000

    Fix2 | Print List | Luigi A

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    item = 0
+    for i in range(x):
+        try:
+            print(my_list[i], end='')
+            item += 1
+        except IndexError:
+            pass
+    print()
+    return(item)
