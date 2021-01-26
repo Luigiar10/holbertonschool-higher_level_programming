708f1aa3dcfa94c32593f6f2366383b8b77d5919
Luigi Arrieta <luigi10arrieta@gmail.com>
2021-01-26 19:10:02 +0000

    Fix3 | Divisible by 2 | Luigi A

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    if not my_list:
-        return (None)
-    numbers = my_list[:]
-    for i in range(0, len(my_list)):
-        if my_list[i] % 2 == 0:
-            numbers[i] == True
+    list_new = []
+    for i in my_list:
+        if i % 2 == 0:
+            list_new.append(True)
         else:
-            numbers[i] == False
-    return (numbers)
+            list_new.append(False)
+    return list_new
