871906aa93eae0ff36dd808f8fa7844dae2e555c
Luigi Arrieta <luigi10arrieta@gmail.com>
2021-01-27 16:05:13 +0000

    Exe1 | Class Square | Luigi A

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Square based in 0-square.py"""
+
+
+class Square:
+       """Class based in 0-square.py"""
+       def __init__(self, size):
+              self.__size = size
+       
