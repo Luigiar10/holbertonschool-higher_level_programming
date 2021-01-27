c5e3a1f81d8c9d723beb3d16720a935ddd3140cd
Luigi Arrieta <luigi10arrieta@gmail.com>
2021-01-27 15:34:03 +0000

    Class And Objects

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-       list2 = ""
-       for i in list2:
+       new_string = str
+       for i in new_string:
               if i == n:
-                     list2.remove(n)
-       return (list2)
+                     new_string[i].remove()
+       return new_string
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,11 @@
+# Class and Objects
+
+## Class
+A class is a code template for creating objects. In python a clas is created by the keyword class
+###
+       class Example:
+
+## Objects
+An object is created using the constructor of the class
+###
+       Instance = example()
