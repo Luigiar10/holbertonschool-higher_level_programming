a51487afce988b0ee80b06a55b2ff2629842046b
Luigi Arrieta <luigi10arrieta@gmail.com>
2021-02-11 02:53:24 +0000

    Fix2 | 2 Square | Luigi Arrieta

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Square based in 0-square.py"""
+
+
+class Square:
+       """inizialiation and display of messages"""
+       def __init__(self, size=0):
+              if zize != :
+                     try:
+                            
+                     print("size must be an integer")
+              else:
+                     print("size must be >= 0 You are not allowed to import any module")
